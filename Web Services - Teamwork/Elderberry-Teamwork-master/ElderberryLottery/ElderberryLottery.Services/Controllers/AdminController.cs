b755f54f63c35a0e49248249303c96e07a3d350d
Viktor90 <viktor102690@gmail.com>
2014-10-16 12:22:32 +0300

    Web Services Teamwork

---
+++ Web Services - Teamwork/Elderberry-Teamwork-master/ElderberryLottery/ElderberryLottery.Services/Controllers/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using ElderberryLottery.Models;
+using ElderberryLottery.Services.Models;
+using ElderberryLotttery.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ElderberryLottery.Services.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : ApiController
+    {
+        private IElderberryLotteryData data;
+
+        public AdminController(IElderberryLotteryData data)
+        {
+            this.data = data;
+        }
+
+        // GET: Api/Admin/AddCode
+        [HttpPost]
+        public IHttpActionResult AddCode(AddCodeModel code)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var codeExists = this.data.GameCodes.All().FirstOrDefault(c => c.Value == code.Value);
+
+            if (codeExists != null)
+            {
+                return BadRequest("Code already exists!");
+            }
+
+            this.data.GameCodes.Add(new GameCode { Value = code.Value, IsWinning = code.IsWinning });
+            this.data.SaveChanges();
+
+            return Ok("Code successfully added!");
+        }
+
+        [HttpGet]
+        public IHttpActionResult AllCodes()
+        {
+            var allCodes = this.data.GameCodes.All().ToList();
+            return Ok(allCodes);
+        }
+
+        [HttpGet]
+        public IHttpActionResult AllUsers()
+        {
+            var allUsers = this.data.Users.All().ToList();
+            return Ok(allUsers);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteUser(DeleteUserModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            
+            var userToDelete = this.data.Users.All().FirstOrDefault(u => u.UserName == user.UserName);
+
+            if (userToDelete == null)
+            {
+                return BadRequest();
+            }
+
+            this.data.Users.Delete(userToDelete);
+            this.data.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Web Services - Teamwork/Elderberry-Teamwork-master/ElderberryLottery/ElderberryLottery.Services/Models/AddCodeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElderberryLottery.Services.Models
+{
+    public class AddCodeModel
+    {
+        [Required]
+        [MinLength(10)]
+        [MaxLength(10)]
+        public string Value { get; set; }
+
+        [Required]
+        public bool IsWinning { get; set; }
+    }
+}
